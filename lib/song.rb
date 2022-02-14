92c746e51fae9d7c09ee6a665051fd0ebae9bd4e
Bridgett Wheeler <bridgettwheeler@gmail.com>
2022-02-14 10:28:27 -0500

    getting help

---
+++ lib/song.rb
@@ -0,0 +1,28 @@
+class Song
+    attr_accessor :name, :artist, :genre
+
+    
+    
+    @@count = 0
+    @@artists = 
+    @@genres = []
+
+    def self.count
+        @@count
+    end
+
+    def self.genre
+        genres << genre
+        
+    end
+
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+    end
+
+
+
+end
